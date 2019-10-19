02f77a9b508040a44940be30d5c75db080f037b1
Helloyi He <open.hfc@gmail.com>
2019-10-19 08:04:12 +0800

    feat: add some must api

--- must.go
+++ must.go
@@ -1,5 +1,6 @@
 package table
 
+// MustInt must api for Int()
 func (t *Table) MustInt() int {
 	i, err := t.Int()
 	if err != nil {
@@ -8,6 +9,7 @@ func (t *Table) MustInt() int {
 	return i
 }
 
+// MustInt8 must api for Int8()
 func (t *Table) MustInt8() int8 {
 	i, err := t.Int8()
 	if err != nil {
@@ -16,6 +18,7 @@ func (t *Table) MustInt8() int8 {
 	return i
 }
 
+// MustInt16 must api for Int16()
 func (t *Table) MustInt16() int16 {
 	i, err := t.Int16()
 	if err != nil {
@@ -24,6 +27,7 @@ func (t *Table) MustInt16() int16 {
 	return i
 }
 
+// MustInt32 must api for Int32()
 func (t *Table) MustInt32() int32 {
 	i, err := t.Int32()
 	if err != nil {
@@ -32,6 +36,7 @@ func (t *Table) MustInt32() int32 {
 	return i
 }
 
+// MustInt64 must api for Int64()
 func (t *Table) MustInt64() int64 {
 	i, err := t.Int64()
 	if err != nil {
@@ -40,6 +45,7 @@ func (t *Table) MustInt64() int64 {
 	return i
 }
 
+// MustFloat32 must api for Float32()
 func (t *Table) MustFloat32() float32 {
 	val, err := t.Float32()
 	if err != nil {
@@ -48,6 +54,7 @@ func (t *Table) MustFloat32() float32 {
 	return val
 }
 
+// MustFloat64 must api for Float64()
 func (t *Table) MustFloat64() float64 {
 	val, err := t.Float64()
 	if err != nil {
@@ -56,6 +63,7 @@ func (t *Table) MustFloat64() float64 {
 	return val
 }
 
+// MustGet must api for Get
 func (t *Table) MustGet(k interface{}) *Table {
 	val, err := t.Get(k)
 	if err != nil {
@@ -64,10 +72,38 @@ func (t *Table) MustGet(k interface{}) *Table {
 	return val
 }
 
+// MustMap must api for Map
 func (t *Table) MustMap() map[*Table]*Table {
 	tm, err := t.Map()
 	if err != nil {
 		panic(err)
 	}
 	return tm
+}
+
+// MustSlice must api for Slice
+func (t *Table) MustSlice() []*Table {
+	ts, err := t.Slice()
+	if err != nil {
+		panic(err)
+	}
+	return ts
+}
+
+// MustAList must api for AList
+func (t *Table) MustAList() [][2]*Table {
+	tl, err := t.AList()
+	if err != nil {
+		panic(err)
+	}
+	return tl
+}
+
+// MustPList must api for PList
+func (t *Table) MustPList() []*Table {
+	tl, err := t.PList()
+	if err != nil {
+		panic(err)
+	}
+	return tl
 }
